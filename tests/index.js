f82de6c10f22665a7bd5804a6a4ddd35ea40c3fb
Josh Levine <josh.levine1@gmail.com>
2015-03-20 16:01:01 -0700

    More tests, standardized mocks/testing values

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ var should = require('should');
 var mocks = require('./mocks');
 
 describe('NetatmoClient', function() {
-    describe('constructor', function() {
+    describe('NetatmoClient (constructor)', function() {
         it('should return an object when both client id and client secret are passed', function() {
             var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
             should(client).be.an.instanceOf(NetatmoApi);
@@ -11,6 +11,8 @@ describe('NetatmoClient', function() {
         it('should throw an error if either client id or client secret are not passed', function() {
             try {
                 new NetatmoApi({});
+                new NetatmoApi({client_id:'test123'});
+                new NetatmoApi({client_secret:'test123'});
             } catch (error) {
                 return;
             }
@@ -25,6 +27,81 @@ describe('NetatmoClient', function() {
             throw new Error("Failed to throw error");
         });
     });
+    describe('request', function() {
+        it('should throw an error if no path is given', function() {
+           var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
+           try {
+               client.request();
+           } catch (error) {
+               return;
+           }
+           throw new Error('Error not caught');
+        });
+        it('should resolve a valid response object if the path and options are valid', function() {
+            var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
+            return client.request('/valid/api/path', { access_token: 'valid_access_token' })
+                .then(function (response) {
+                    should(response).have.property('status','ok');
+                    should(response).have.property('body');
+                });
+        });
+        it('should throw a NotFoundError if the api path is not valid', function(done) {
+            var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
+            client.request('/invalid/api/path', {})
+                .then(function () {
+                    done(new Error('promise resolved when it should have rejected'));
+                })
+                .catch(NetatmoApi.errors.NotFoundError, function() {
+                    done();
+                })
+                .catch(done);
+        });
+        it('should throw a AccessTokenExpiredError if the access_token passed is expired', function(done) {
+            var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
+            client.request('/valid/api/path', { access_token: 'expired_access_token' })
+                .then(function () {
+                    done(new Error('promise resolved when it should have rejected'));
+                })
+                .catch(NetatmoApi.errors.AccessTokenExpiredError, function() {
+                    done();
+                })
+                .catch(done);
+        });
+        it('should throw a AccessTokenInvalidError if the access_token passed is expired', function(done) {
+            var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
+            client.request('/valid/api/path', { access_token: 'invalid_access_token' })
+                .then(function () {
+                    done(new Error('promise resolved when it should have rejected'));
+                })
+                .catch(NetatmoApi.errors.AccessTokenInvalidError, function() {
+                    done();
+                })
+                .catch(done);
+        });
+    });
+    describe('callback based response handling', function() {
+        it('should pass the error as the first argument to a callback', function(done) {
+            var client = new NetatmoApi({ clientId: 'invalid', clientSecret: 'invalid' });
+            client.getToken({}, function (err, result) {
+                if (err) return done();
+                done(new Error('Error not received'));
+            });
+        });
+        it('should pass the result as the 2nd argument to a callback', function(done) {
+            var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
+            client.getToken({
+                grant_type: 'password',
+                username: 'valid',
+                password: 'valid'
+            }, function (err, token) {
+                if (err) return done(err);
+                should(token).have.property('access_token');
+                should(token).have.property('expires_in');
+                should(token).have.property('refresh_token');
+                done();
+            });
+        });
+    });
     describe('getToken', function() {
         it('should throw an InvalidClientError if client id and client secret are invalid', function(done) {
             var client = new NetatmoApi({ clientId: 'invalid', clientSecret: 'invalid' });
@@ -34,9 +111,9 @@ describe('NetatmoClient', function() {
                 done();
             }).catch(done);
         });
-        describe('grant_type = password', function() {
+        describe('{ grant_type: \'password\' }', function() {
             it('should resolve an access token if the username/password is valid', function(done) {
-                var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
+                var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
                 client.getToken({
                     grant_type: 'password',
                     username: 'valid',
@@ -51,7 +128,7 @@ describe('NetatmoClient', function() {
                 });
             });
             it('should throw an InvalidGrantError if the username/password is wrong', function(done) {
-                var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
+                var client = new NetatmoApi({ clientId: 'valid', clientSecret: 'valid' });
                 client.getToken({
                     grant_type: 'password',
                     username: 'invalid',
@@ -62,32 +139,6 @@ describe('NetatmoClient', function() {
                     done();
                 }).catch(done);
             });
-        });
-    });
-    describe('getUser', function() {
-        it('should resolve a user when a valid acess_token is passed', function(done) {
-            var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
-            client.getUser({ access_token: 'valid_access_token' }).then(function (user) {
-                should(user).have.property('status','ok');
-                should(user).have.property('body');
-                done();
-            }).catch(done);
-        });
-        it('should throw an AccessTokenExpiredError when an expired token is passed', function(done) {
-            var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
-            client.getUser({ access_token: 'expired_access_token' }).then(function () {
-                done(new Error('promise resolved when it should have rejected'));
-            }).catch(NetatmoApi.errors.AccessTokenExpiredError, function() {
-                done();
-            }).catch(done);
-        });
-        it('should throw an InvalidAccessTokenError when an invalid token is passed', function(done) {
-            var client = new NetatmoApi({ clientId: 'test', clientSecret: 'test' });
-            client.getUser({ access_token: 'invalid_access_token' }).then(function () {
-                done(new Error('promise resolved when it should have rejected'));
-            }).catch(NetatmoApi.errors.InvalidAccessTokenError, function() {
-                done();
-            }).catch(done);
         });
     });
 });
--- tests/mocks.js
+++ tests/mocks.js
@@ -2,23 +2,25 @@ var nock = require('nock');
 
 var netatmo = nock('https://api.netatmo.net');
 
-//
-// Client Credentials Authorization
-//
+netatmo.persist();
 
-// Successful
+/**
+ * Successful Client Authorization
+ */
 netatmo.post('/oauth2/token', {
     grant_type: 'password',
     username: 'valid',
     password: 'valid',
-    client_id: 'test',
-    client_secret: 'test',
+    client_id: 'valid',
+    client_secret: 'valid',
     scope: 'read_station read_thermostat write_thermostat'
 }).reply(200,
     JSON.parse("{\"access_token\":\"542080451c775998ef579935|ea4855f21a6207b8750c487b773fefb5\",\"expires_in\": 10800,\"expire_in\": 10800,\"scope\": [ \"read_station\", \"read_thermostat\", \"write_thermostat\" ],\"refresh_token\": \"542080451c775998ef579935|b4231af13b4c711faa40e1eb57e1efcd\" }")
 );
 
-// Invalid client id/secret
+/**
+ * Invalid client id and/or client secret
+ */
 netatmo.post('/oauth2/token', {
     client_id: 'invalid',
     client_secret: 'invalid',
@@ -27,11 +29,13 @@ netatmo.post('/oauth2/token', {
     error: "invalid_client"
 });
 
-// Invalid username/password
+/**
+ * Invalid username and/or password
+ */
 netatmo.post('/oauth2/token', {
     grant_type: 'password',
-    client_id: 'test',
-    client_secret: 'test',
+    client_id: 'valid',
+    client_secret: 'valid',
     username: 'invalid',
     password: 'invalid',
     scope: 'read_station read_thermostat write_thermostat'
@@ -39,43 +43,41 @@ netatmo.post('/oauth2/token', {
     error: "invalid_grant"
 });
 
-netatmo.get('/api/getuser?access_token=valid_access_token').reply(200, {
-    "status": "ok",
-    "body": {
-        "_id": "542080451c775998ef579935",
-        "administrative": {
-            "country": "US",
-            "reg_locale": "en-US",
-            "lang": "en-US",
-            "unit": 1,
-            "windunit": 1,
-            "pressureunit": 1,
-            "feel_like_algo": 1
-        },
-        "date_creation": {
-            "sec": 1411416133,
-            "usec": 0
-        },
-        "devices": [
-            "70:ee:50:03:9d:e6"
-        ],
-        "facebook_like_displayed": true,
-        "mail": "josh.levine1@gmail.com",
-        "timeline_not_read": 1,
-        "usage_mark": 0.42572773404963
-    }
-});
-
-netatmo.get('/api/getuser?access_token=expired_access_token').reply(403, {
+/**
+ * Expired Access Token
+ */
+netatmo.get('/valid/api/path?access_token=expired_access_token').reply(403, {
     error: {
-        code: 3
+        code: 3,
+        message: "Expired access token"
     }
 });
 
-netatmo.get('/api/getuser?access_token=invalid_access_token').reply(403, {
+/**
+ * Invalid access token
+ */
+netatmo.get('/valid/api/path?access_token=invalid_access_token').reply(403, {
     error: {
         code: 2,
         "message" : "Invalid access token"
+    }
+});
+
+/**
+ * Successful api call
+ */
+netatmo.get('/valid/api/path?access_token=valid_access_token').reply(200, {
+    "status": "ok",
+    "body": {}
+});
+
+/**
+ * 404 not found api call
+ */
+netatmo.get('/invalid/api/path').reply(404, {
+    "error" : {
+        "code" : 404,
+        "message" : "Not Found"
     }
 });
 
